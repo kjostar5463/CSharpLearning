d0749f522e1e47740b36a886c9b7396d03cf99e7
kjostar5463 <kkrc5463@gmail.com>
2023-03-31 19:48:08 +0900

    3/31

--- BoxingAndUnBoxing/BackJoon/Program.cs
+++ BoxingAndUnBoxing/BackJoon/Program.cs
@@ -6,12 +6,72 @@ namespace BackJoon
     {
         static void Main(string[] args)
         {
-            int T = int.Parse(Console.ReadLine());
-            for(int i = 0; i < T/4; i++)
+            byte N = byte.Parse(Console.ReadLine());
+            byte count = 0;
+            byte[] check = new byte[26];
+            string []word = new string[N];
+            
+            for(byte i = 0; i < N; i++)
             {
-                Console.Write("long ");
+                word[i] = Console.ReadLine();
             }
-            Console.WriteLine("int");            
+            
+            for (byte i = 0; i < N; i++)
+            {
+                if (word[i].Length == 1)
+                {
+                    count++;
+                    continue;
+                }
+                for (byte j = 0; j < 26; j++)
+                {
+                    check[j] = 0;
+                }
+                for (int k = 0; k < word[i].Length; k++)
+                {
+                    int index = word[i][k] - 'a';
+                    if (check[index] == 0)
+                    {
+                        if (k < word[i].Length - 1)
+                        {
+                            if (word[i][k] != word[i][k + 1])
+                                check[index] = 1;
+                        }
+                        else
+                            check[index] = 1;
+                    }
+                    else
+                    {
+                        count--;
+                        break;
+                    }
+                }
+            
+                count++;
+            
+            }
+            
+            Console.WriteLine(count);
+            
+            //for (int i = 0; i < word.Length; i++)
+            //{
+            //    for (int j = i; j < word.Length; j++)
+            //    {
+            //        if (j - i > 1)
+            //        {
+            //            if (word[i] == word[j])
+            //            {
+            //                Console.WriteLine(false);
+            //            }
+            //        }
+            //        if (word[i] == word[j])
+            //        {
+            //            i += j - i;
+            //        }
+            //    }
+            //}
+            //Console.WriteLine(true);
+            
         }
     }
 }
---
+++ BoxingAndUnBoxing/StringBuilder/Program.cs
@@ -0,0 +1,124 @@
+﻿namespace StringBuilder
+{
+    struct DataTable
+    {
+        public int memory;
+        public int[] array;
+    }
+
+    class A
+    {
+        public A()
+        {
+            Console.WriteLine("A");
+        }
+    }
+
+    class B
+    {
+        public B(string name)
+        {
+            Console.WriteLine(name);
+        }
+    }
+
+    class Language<T> where T : new()
+    {
+        // where T : struct
+        // null 허용 X
+
+        // where T : class
+        // 참조 형식
+
+        // where T : new()
+        // 매개 변수가 없는 public
+
+        // where T : unmanaged
+        // struct를 포함하지 않음
+        // T는 null이 아닌 비관리형 형식
+        // char, byte, bool, int , float, double, decimal
+
+        // where T : <base class name>
+        // 지정된 기본 클래스 이거나 이 클래스에서 파생된 클래스
+
+        private T data; 
+
+        public  T Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+    }
+
+    internal class Program
+    { 
+        static void Close()
+        {
+            Console.WriteLine("Function");
+        }
+        static void Open(int power)
+        {
+            Console.WriteLine("문을 여는 힘 : " + power);
+        }
+
+        static int score()
+        {
+
+            return 100;
+        }
+        static int Result(int value)
+        {
+            return value;
+        }
+
+        static void Main(string[] args)
+        {
+            #region Action
+            Action action = Close;
+            Action<int> intAction = Open;
+
+            action();
+
+            action = () => { Console.WriteLine("로그인 성공"); };
+            action();
+
+            intAction(100);
+
+            intAction = (x) => { Console.WriteLine("로그인 시도 횟수 : " + x); };
+            intAction(3);
+            #endregion
+
+            #region Func
+            Func<int> func;
+            // Func는 <반환형, 매개변수, ........>
+            Func<int, int> intFunc;
+
+            func = score;
+
+            Console.WriteLine("Score " +  func());
+
+            func = () => { return 999; };
+            Console.WriteLine("Score " + func());
+
+            intFunc = Result;
+
+            Console.WriteLine("Result " + intFunc(1000));
+
+            intFunc = (x) => { return x; };
+            Console.WriteLine("Result " + intFunc(222));
+            #endregion
+
+            #region 제약 조건 (where)
+            Language<int> language = new Language<int>();
+            language.Data = 100;
+
+            Console.WriteLine(language.Data);
+
+            //Language<string> language1 = new Language<string>();
+            //Language<B> language2 = new Language<B>();
+
+            #endregion
+
+        }
+    }
+}
